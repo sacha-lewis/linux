0c18787baf6f83b8fb668851dbf63d4ceb7cc714
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2025-12-19 16:55:48 -0500

    Initial POPos installation

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "=== Bitwarden ==="
+./bitwarden.sh
+
+echo "=== Docker ==="
+./docker.sh
+
+echo "=== Firefox Developer ==="
+./firefox-developer.sh
+
+echo "=== Jetbrains ==="
+./jetbrains.sh
+
