e3aa73e6cd623a522d276f538608d328d61a4158
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2026-06-07 22:19:37 -0400

    Switching to a more simply solution - Added a basic concept script -> Will refine later

--- obsidian.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub md.obsidian.Obsidian
--- virtualBox.sh
+++
@@ -1,7 +0,0 @@
-# Enable clipboard when OFF
-
-# then turn on
-
-# install guest additions
-
-# sudo /media/sacha-lewis/VB.../VBoxLinuxAdditions.run
