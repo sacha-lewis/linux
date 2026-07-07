f9c1f9e33bf0a89cc0db7a61f6957bdd9a17036b
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2026-07-07 16:31:16 -0400

    setting up aurora

--- chrome.sh
+++ chrome.sh
@@ -5,3 +5,11 @@
 
 # open to install
 https://chromewebstore.google.com/detail/bitwarden-password-manage/nngceckbapebfimnlniiiahkandclblb?hl=en-GB&utm_source=ext_sidebar
+
+
+
+https://chromewebstore.google.com/detail/pip-picture-in-picture/nalkmonnmldhpfcpdlbdpljlaajlaphh?hl=en-US&utm_source=ext_sidebar
+
+
+
+https://chromewebstore.google.com/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh?hl=en-US&utm_source=ext_sidebar
